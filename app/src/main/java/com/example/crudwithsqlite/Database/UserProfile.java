91c6b81083ecc2385a2bcbae5f4a8d0123db177d
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-06-03 02:32:44 +0530

    Implement User profile

---
+++ app/src/main/java/com/example/crudwithsqlite/Database/UserProfile.java
@@ -0,0 +1,16 @@
+package com.example.crudwithsqlite.Database;
+
+import android.provider.BaseColumns;
+
+public final class UserProfile {
+    private UserProfile() {}
+
+    public static class Users implements BaseColumns {
+        public static final String TABLE_NAME = "UserInfo";
+        public static final String COLUMN_1 = "userName";
+        public static final String COLUMN_2 = "dateOfBirth";
+        public static final String COLUMN_3 = "password";
+        public static final String COLUMN_4 = "gender";
+
+    }
+}
